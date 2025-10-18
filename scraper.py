28655cd97d9ea21e4e2b493ba5a86cbf64dc46c5
Brandon Rachal <brandonrachalxula@gmail.com>
2025-10-18 12:26:07 -0500

    Added pretty welcome message using ASCII art to scraper.py

--- scraper.py
+++ scraper.py
@@ -1,5 +1,14 @@
 def main():
-    print("\n\nHello, World from scraper.py!\n\n")
+    print(r"""
+ __        __   _
+ \ \      / /__| | ___ ___  _ __ ___   ___
+  \ \ /\ / / _ \ |/ __/ _ \| '_ ` _ \ / _ \
+   \ V  V /  __/ | (_| (_) | | | | | |  __/
+    \_/\_/ \___|_|\___\___/|_| |_| |_|\___|
+                                                    
+✨ Welcome to Brandon's Scraper ✨
+Let's build something awesome today!
+""")
 
 
 if __name__ == "__main__":
